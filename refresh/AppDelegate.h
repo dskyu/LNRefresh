fa1849ad4052f87bb7b8a5d2cb2014461712ed9a
dskyu <dskyu2004@gmail.com>
2013-09-25 21:01:04 +0800

    Initial Commit

---
+++ refresh/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  refresh
+//
+//  Created by bunnydu on 13-9-25.
+//  Copyright (c) 2013年 diditech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ refresh/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  refresh
+//
+//  Created by bunnydu on 13-9-25.
+//  Copyright (c) 2013年 diditech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
